92d72b77894174e4203e53c9b91ee51caacd2cc8
Andrii Kicha <akicha@star.global>
2022-10-11 21:57:01 +0300

    Initial Commit

---
+++ multithreading lab 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multithreading lab 1
+//
+//  Created by Andrii Kicha on 11.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
