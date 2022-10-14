a94963cf73dc7265b67ba353dc056ee12eb03887
Andrii Kicha <akicha@star.global>
2022-10-14 20:49:57 +0300

    squash

--- multithreading lab 1/main.cpp
+++ multithreading lab 1/main.cpp
@@ -40,8 +40,13 @@ class StateMachine {
     int mP3;
     int mP4;
     
+    string mInitiator;
+    
+//    StateMachine *mPrevState;
+//    StateMachine *mNextState;
+    
 public:
-    StateMachine(int aX, int aY, int aZ, int aP1, int aP2, int aP3, int aP4) {
+    StateMachine(int aX, int aY, int aZ, int aP1, int aP2, int aP3, int aP4, string aInitiator) {
         this->mX = aX;
         this->mY = aY;
         this->mZ = aZ;
@@ -50,11 +55,15 @@ public:
         this->mP2 = aP2;
         this->mP3 = aP3;
         this->mP4 = aP4;
+        
+        this->mInitiator = aInitiator;
     }
     
-    void makeNextState(int aX, int aY, int aZ, int aP1, int aP2, int aP3, int aP4) {
+    void makeNextState(int aX, int aY, int aZ, int aP1, int aP2, int aP3, int aP4, string aInitiator) {
         m.lock();
-        StateMachine s(aX, aY, aZ, aP1, aP2, aP3, aP4);
+        StateMachine s(aX, aY, aZ, aP1, aP2, aP3, aP4, aInitiator);
+        // s.setPrevState(*this);
+        // this->mNextState = &s;
         this->mState.push_back(s);
         m.unlock();
     }
@@ -88,36 +97,48 @@ public:
             }
         }
     }
+//
+//    void setPrevState(StateMachine &s) {
+//        this->mPrevState = &s;
+//    }
+//
+//    StateMachine getPreviousState() {
+//        return *this->mPrevState;
+//    }
+//
+//    StateMachine getNextState() {
+//        return *this->mNextState;
+//    }
 };
 
 // Privisioning the state machine with the inital state
-StateMachine sm(0, 0, 0, 0, 0, 0, 0);
+StateMachine sm(0, 0, 0, 0, 0, 0, 0, "");
 
 void thread1() {
     x=0;
     p1 = 11;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p11");
     
     y=0;
     p1 = 12;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p12");
     
     z=0;
     p1 = 13;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p13");
     
     while (n < MAX_ITERATIONS) {
         x=1;
         p1 = 14;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p14");
         
         x=2;
         p1 = 15;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p15");
         
         x=z;
         p1 = 16;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p16");
         
         n++;
     }
@@ -128,28 +149,28 @@ void thread1() {
 void thread2() {
     x=1;
     p2 = 21;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p21");
     
     y=1;
     p2 = 22;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p22");
     
     z=1;
     p2 = 23;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p23");
     
     while (n < MAX_ITERATIONS) {
         y=x;
         p2 = 24;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p24");
         
         y=3;
         p2 = 25;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p25");
         
         z=1;
         p3 = 26;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p26");
         
         n++;
     }
@@ -160,28 +181,28 @@ void thread2() {
 void thread3() {
     x=2;
     p3 = 31;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p31");
     
     y=2;
     p3 = 32;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p32");
     
     z=2;
     p3 = 33;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p33");
 
     while (n < MAX_ITERATIONS) {
         z=2;
         p3 = 34;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p34");
         
         z=y;
         p3 = 35;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p35");
         
         x=1;
         p3 = 36;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p36");
         
         n++;
     }
@@ -192,28 +213,28 @@ void thread3() {
 void thread4() {
     x=0;
     p4 = 41;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p41");
     
     y=1;
     p4 = 42;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p42");
     
     z=2;
     p4 = 43;
-    sm.makeNextState(x, y, z, p1, p2, p3, p4);
+    sm.makeNextState(x, y, z, p1, p2, p3, p4, "p43");
     
     while (n < MAX_ITERATIONS) {
         y=3;
         p4 = 44;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p44");
         
         y=5;
         p4 = 45;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p45");
         
         x=7;
         p4 = 46;
-        sm.makeNextState(x, y, z, p1, p2, p3, p4);
+        sm.makeNextState(x, y, z, p1, p2, p3, p4, "p46");
         
         n++;
     }
@@ -233,6 +254,21 @@ void log() {
     for (StateMachine smn : s) {
         cout << smn.toString() << endl;
     }
+    
+//    for (StateMachine smn : s) {
+//        cout << "-----------------------" << endl;
+//
+//        cout << "The captured state: ";
+//        cout << smn.toString() << endl;
+//
+//        cout << "The previous state: ";
+//        cout << smn.getPreviousState().toString() << endl;
+//
+////        cout << "The next state: ";
+////        cout << smn.getNextState().toString() << endl;
+//
+//        cout << "----------------------" << endl;
+//    }
 }
 
 
